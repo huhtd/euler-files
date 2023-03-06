ef4f1aa0dc74fee320a7683fc0bd31e9d83ad7a2
Bogdan Dubinin <bogdandubinin@student-net-hg-3583.intern.ethz.ch>
2023-03-06 17:14:29 +0100

    blabla

--- project1-code/hello_read.cpp
+++ project1-code/hello_read.cpp
@@ -5,15 +5,16 @@
  
 int main(int argc, char* argv[])
 {
-
+    const char* uname = std::getenv("USER");
     const char* hname = std::getenv("HOSTNAME");
+    const char* home = std::getenv("HOME");
 
-
-    if (hname)
+    if (uname && hname && home)
     {
-
+        std::cout << "Your name is:           " << argv[1] << '\n';
+        std::cout << "Your username is:       " << uname << '\n';
         std::cout << "Your hostname is:       " << hname << '\n';
-
+        std::cout << "Your home directory is:    " << home << '\n';
     }
     else
     {
