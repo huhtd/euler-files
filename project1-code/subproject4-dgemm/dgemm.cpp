2ea044e301037084dbfb35311b0c13d5fd338dba
Bogdan Dubinin <bogdandubinin@student-net-vpn-0485.intern.ethz.ch>
2023-03-10 23:33:43 +0100

    added files

--- project1-code/subproject4-dgemm/dgemm.cpp
+++ project1-code/subproject4-dgemm/dgemm.cpp
@@ -6,7 +6,7 @@
 #include <limits>
 #include <assert.h>
 #include <iostream>
-#include <immintrin.h>  // AVX
+#include <immintrin.h>  // AVX, for SIMD intrinsics
 
 #include "Stopwatch.h"
 
@@ -24,7 +24,27 @@ void dgemm(double* A, double* B, double* C) {
 
 void dgemm_opt(double* A, double* B, double* C) {
   // TODO: write your code
-}
+  // Processors has L1 chache 32KB, with cache block size 64bytes (8 doubles)
+  // L1 cache size is 32 KB = 8 B * 4K --> 2^12 = 4096 doubles fits in cache
+
+  __m256d va, vb, vc, vd,ve;
+  for (int n = 0; n < N; n+=1) {
+    for (int k = 0; k < K; k+=1) {
+      vb = _mm256_broadcast_sd(B + n*K+k ); // matrix_b[j][k]
+      for (int m = 0; m < M; m+=4) {
+        va = _mm256_load_pd(A + k*M+m); // matrix_b[j][k]
+        // // va = _mm256_i64gather_pd(&A[k*M + m], _mm256_set_epi64x(3*M, 2*M, M,0), 8); // matrix_a[i][k]
+
+        // // add and multiply
+        vd = _mm256_load_pd( C+n*M+m);
+        vd = _mm256_fmadd_pd(va, vb,vd);
+        _mm256_store_pd(C+n*M+m, vd);
+        // C[n*M + m] += _mm256_cvtsd_f64(vc);
+      }
+      // add
+    }
+  }
+
 
 int main(int argc, char** argv) {
   int repetitions = 10000;
