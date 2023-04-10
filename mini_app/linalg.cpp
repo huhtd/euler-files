c51cf2f9a9a64abceeb6d0e37127fce3263913aa
Bogdan Dubinin <bogdou@gmail.com>
2023-04-10 15:25:10 +0200

    linalg.cpp changed

--- mini_app/linalg.cpp
+++ mini_app/linalg.cpp
@@ -10,6 +10,7 @@
 #include "operators.h"
 #include "stats.h"
 #include "data.h"
+#include <omp.h>
 
 namespace linalg {
 
@@ -48,110 +49,112 @@ void cg_init(int nx)
 //  blas level 1 reductions
 ////////////////////////////////////////////////////////////////////////////////
 
-// computes the inner product of x and y
-// x and y are vectors on length N
-double hpc_dot(Field const& x, Field const& y, const int N)
+#pragma omp parallel
 {
-    double result = 0;
+    // computes the inner product of x and y
+    // x and y are vectors on length N
+    double hpc_dot(Field const& x, Field const& y, const int N)
+    {
+        double result = 0;
+        #pragma omp for reduction(*:result)
+        for (int i = 0; i < N; i++)
+            result += x[i] * y[i];
 
-    for (int i = 0; i < N; i++)
-        result += x[i] * y[i];
+        return result;
+    }
 
-    return result;
+    // computes the 2-norm of x
+    // x is a vector on length N
+    double hpc_norm2(Field const& x, const int N)
+    {
+        double result = 0;
+
+        #pragma omp for reduction(+:result)
+        for(int i = 0; i < N; i++)
+            result += x[i] * x[i];
+
+        return sqrt(result);
+    }
+
+    // sets entries in a vector to value
+    // x is a vector on length N
+    // value is a scalar
+    void hpc_fill(Field& x, const double value, const int N)
+    {
+        #pragma omp for
+        for(int i = 0; i < N; i++)
+            x[i] = value;
+        
+
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //  blas level 1 vector-vector operations
+    ////////////////////////////////////////////////////////////////////////////////
+
+    // computes y := alpha*x + y
+    // x and y are vectors on length N
+    // alpha is a scalar
+    void hpc_axpy(Field& y, const double alpha, Field const& x, const int N)
+    {
+        #pragma omp for
+        for(int i = 0; i < N; i++)
+            y[i] += alpha * x[i];
+    }
+
+    // computes y = x + alpha*(l-r)
+    // y, x, l and r are vectors of length N
+    // alpha is a scalar
+    void hpc_add_scaled_diff(Field& y, Field const& x, const double alpha,
+        Field const& l, Field const& r, const int N)
+    {
+        #pragma omp for
+        for(int i = 0; i < N; i++)
+            y[i] = x[i] + alpha * (l[i]-r[i]);
+
+    }
+
+    // computes y = alpha*(l-r)
+    // y, l and r are vectors of length N
+    // alpha is a scalar
+    void hpc_scaled_diff(Field& y, const double alpha,
+        Field const& l, Field const& r, const int N)
+    {
+        #pragma omp for
+        for(int i = 0; i < N; i++)
+            y[i] = alpha * (l[i]-r[i]);
+    }
+
+    // computes y := alpha*x
+    // alpha is scalar
+    // y and x are vectors on length n
+    void hpc_scale(Field& y, const double alpha, Field& x, const int N)
+    {
+        #pragma omp for
+        for(int i = 0; i < N; i++)
+            y[i] = alpha * x[i];
+    }
+
+    // computes linear combination of two vectors y := alpha*x + beta*z
+    // alpha and beta are scalar
+    // y, x and z are vectors on length n
+    void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
+        Field const& z, const int N)
+    {
+        #pragma omp for
+        for(int i = 0; i < N; i++)
+            y[i] = alpha * x[i] + beta * z[i];
+    }
+
+    // copy one vector into another y := x
+    // x and y are vectors of length N
+    void hpc_copy(Field& y, Field const& x, const int N)
+    {
+        #pragma omp for
+        for(int i = 0; i < N; i++)
+            y[i] = x[i];
+    }
 }
-
-// computes the 2-norm of x
-// x is a vector on length N
-double hpc_norm2(Field const& x, const int N)
-{
-    double result = 0;
-
-   
-    for(int i = 0; i < N; i++)
-        result += x[i] * x[i];
-
-    return sqrt(result);
-}
-
-// sets entries in a vector to value
-// x is a vector on length N
-// value is a scalar
-void hpc_fill(Field& x, const double value, const int N)
-{
-    
-    for(int i = 0; i < N; i++)
-        x[i] = value;
-    
-
-}
-
-////////////////////////////////////////////////////////////////////////////////
-//  blas level 1 vector-vector operations
-////////////////////////////////////////////////////////////////////////////////
-
-// computes y := alpha*x + y
-// x and y are vectors on length N
-// alpha is a scalar
-void hpc_axpy(Field& y, const double alpha, Field const& x, const int N)
-{
-    
-    for(int i = 0; i < N; i++)
-        y[i] += alpha * x[i];
-}
-
-// computes y = x + alpha*(l-r)
-// y, x, l and r are vectors of length N
-// alpha is a scalar
-void hpc_add_scaled_diff(Field& y, Field const& x, const double alpha,
-    Field const& l, Field const& r, const int N)
-{
-    
-    for(int i = 0; i < N; i++)
-        y[i] = x[i] + alpha * (l[i]-r[i]);
-
-}
-
-// computes y = alpha*(l-r)
-// y, l and r are vectors of length N
-// alpha is a scalar
-void hpc_scaled_diff(Field& y, const double alpha,
-    Field const& l, Field const& r, const int N)
-{
-    
-    for(int i = 0; i < N; i++)
-        y[i] = alpha * (l[i]-r[i]);
-}
-
-// computes y := alpha*x
-// alpha is scalar
-// y and x are vectors on length n
-void hpc_scale(Field& y, const double alpha, Field& x, const int N)
-{
-    
-    for(int i = 0; i < N; i++)
-        y[i] = alpha * x[i];
-}
-
-// computes linear combination of two vectors y := alpha*x + beta*z
-// alpha and beta are scalar
-// y, x and z are vectors on length n
-void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
-    Field const& z, const int N)
-{
-    
-    for(int i = 0; i < N; i++)
-        y[i] = alpha * x[i] + beta * z[i];
-}
-
-// copy one vector into another y := x
-// x and y are vectors of length N
-void hpc_copy(Field& y, Field const& x, const int N)
-{
-    
-    for(int i = 0; i < N; i++)
-        y[i] = x[i];
-}
-
 // conjugate gradient solver
 // solve the linear system A*x = b for x
 // the matrix A is implicit in the objective function for the diffusion equation
