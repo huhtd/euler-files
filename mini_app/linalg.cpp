b23f1dacccdbbc94d49d36dfb155b0cadc8d9a29
Bogdan Dubinin <bogdou@gmail.com>
2023-04-10 16:37:29 +0200

    linalg.cpp original

--- mini_app/linalg.cpp
+++ mini_app/linalg.cpp
@@ -1,254 +1,248 @@
-// linear algebra subroutines
-// Ben Cumming @ CSCS
+ //linear algebra subroutines
+ // Ben Cumming @ CSCS
 
-#include <iostream>
+ #include <iostream>
 
-#include <cmath>
-#include <cstdio>
+ #include <cmath>
+ #include <cstdio>
 
-#include "linalg.h"
-#include "operators.h"
-#include "stats.h"
-#include "data.h"
-#include <omp.h>
+ #include "linalg.h"
+ #include "operators.h"
+ #include "stats.h"
+ #include "data.h"
 
-namespace linalg {
+ namespace linalg {
 
-bool cg_initialized = false;
-Field r;
-Field Ap;
-Field p;
-Field Fx;
-Field Fxold;
-Field v;
-Field xold;
+ bool cg_initialized = false;
+ Field r;
+ Field Ap;
+ Field p;
+ Field Fx;
+ Field Fxold;
+ Field v;
+ Field xold;
 
-using namespace operators;
-using namespace stats;
-using data::Field;
+ using namespace operators;
+ using namespace stats;
+ using data::Field;
 
-// initialize temporary storage fields used by the cg solver
-// I do this here so that the fields are persistent between calls
-// to the CG solver. This is useful if we want to avoid malloc/free calls
-// on the device for the OpenACC implementation (feel free to suggest a better
-// method for doing this)
-void cg_init(int nx)
-{
-    Ap.init(nx,nx);
-    r.init(nx,nx);
-    p.init(nx,nx);
-    Fx.init(nx,nx);
-    Fxold.init(nx,nx);
-    v.init(nx,nx);
-    xold.init(nx,nx);
+ // initialize temporary storage fields used by the cg solver
+ // I do this here so that the fields are persistent between calls
+ // to the CG solver. This is useful if we want to avoid malloc/free calls
+ // on the device for the OpenACC implementation (feel free to suggest a better
+ // method for doing this)
+ void cg_init(int nx)
+ {
+     Ap.init(nx,nx);
+     r.init(nx,nx);
+     p.init(nx,nx);
+     Fx.init(nx,nx);
+     Fxold.init(nx,nx);
+     v.init(nx,nx);
+     xold.init(nx,nx);
 
-    cg_initialized = true;
-}
+     cg_initialized = true;
+ }
 
-////////////////////////////////////////////////////////////////////////////////
-//  blas level 1 reductions
-////////////////////////////////////////////////////////////////////////////////
+ ////////////////////////////////////////////////////////////////////////////////
+ //  blas level 1 reductions
+ ////////////////////////////////////////////////////////////////////////////////
 
-// #pragma omp parallel
-{
-}
-{
-    // computes the inner product of x and y
-    // x and y are vectors on length N
-    double hpc_dot(Field const& x, Field const& y, const int N)
-    {
-        double result = 0;
-        
-        {
-        for (int i = 0; i < N; i++)
-            result += x[i] * y[i];
-        }
-        return result;
-    }
+ // computes the inner product of x and y
+ // x and y are vectors on length N
+ double hpc_dot(Field const& x, Field const& y, const int N)
+ {
+     double result = 0;
 
-    // computes the 2-norm of x
-    // x is a vector on length N
-    double hpc_norm2(Field const& x, const int N)
-    {
-        double result = 0;
+     for (int i = 0; i < N; i++)
+         result += x[i] * y[i];
+
+     return result;
+ }
+
+ // computes the 2-norm of x
+ // x is a vector on length N
+ double hpc_norm2(Field const& x, const int N)
+ {
+     double result = 0;
+
+     //TODO
+     for(int i = 0; i < N; i++)
+         result += x[i] * x[i];
+
+     return sqrt(result);
+ }
+
+ // sets entries in a vector to value
+ // x is a vector on length N
+ // value is a scalar
+ void hpc_fill(Field& x, const double value, const int N)
+ {
+     //TODO
+     for(int i = 0; i < N; i++)
+         x[i] = value;
 
 
-        for(int i = 0; i < N; i++)
-            result += x[i] * x[i];
+ }
 
-        return sqrt(result);
-    }
+ ////////////////////////////////////////////////////////////////////////////////
+ //  blas level 1 vector-vector operations
+ ////////////////////////////////////////////////////////////////////////////////
 
-    // sets entries in a vector to value
-    // x is a vector on length N
-    // value is a scalar
-    void hpc_fill(Field& x, const double value, const int N)
-    {
-        
-        for(int i = 0; i < N; i++)
-            x[i] = value;
-        
+ // computes y := alpha*x + y
+ // x and y are vectors on length N
+ // alpha is a scalar
+ void hpc_axpy(Field& y, const double alpha, Field const& x, const int N)
+ {
+     //TODO
+     for(int i = 0; i < N; i++)
+         y[i] += alpha * x[i];
+ }
 
-    }
+ // computes y = x + alpha*(l-r)
+ // y, x, l and r are vectors of length N
+ // alpha is a scalar
+ void hpc_add_scaled_diff(Field& y, Field const& x, const double alpha,
+     Field const& l, Field const& r, const int N)
+ {
+     //TODO
+     for(int i = 0; i < N; i++)
+         y[i] = x[i] + alpha * (l[i]-r[i]);
 
-    ////////////////////////////////////////////////////////////////////////////////
-    //  blas level 1 vector-vector operations
-    ////////////////////////////////////////////////////////////////////////////////
+ }
 
-    // computes y := alpha*x + y
-    // x and y are vectors on length N
-    // alpha is a scalar
-    void hpc_axpy(Field& y, const double alpha, Field const& x, const int N)
-    {
-        
-        for(int i = 0; i < N; i++)
-            y[i] += alpha * x[i];
-    }
+ // computes y = alpha*(l-r)
+ // y, l and r are vectors of length N
+ // alpha is a scalar
+ void hpc_scaled_diff(Field& y, const double alpha,
+     Field const& l, Field const& r, const int N)
+ {
+     //TODO
+     for(int i = 0; i < N; i++)
+         y[i] = alpha * (l[i]-r[i]);
+ }
 
-    // computes y = x + alpha*(l-r)
-    // y, x, l and r are vectors of length N
-    // alpha is a scalar
-    void hpc_add_scaled_diff(Field& y, Field const& x, const double alpha,
-        Field const& l, Field const& r, const int N)
-    {
-        
-        for(int i = 0; i < N; i++)
-            y[i] = x[i] + alpha * (l[i]-r[i]);
+ // computes y := alpha*x
+ // alpha is scalar
+ // y and x are vectors on length n
+ void hpc_scale(Field& y, const double alpha, Field& x, const int N)
+ {
+     //TODO
+     for(int i = 0; i < N; i++)
+         y[i] = alpha * x[i];
+ }
 
-    }
+ // computes linear combination of two vectors y := alpha*x + beta*z
+ // alpha and beta are scalar
+ // y, x and z are vectors on length n
+ void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
+     Field const& z, const int N)
+ {
+     //TODO
+     for(int i = 0; i < N; i++)
+         y[i] = alpha * x[i] + beta * z[i];
+ }
 
-    // computes y = alpha*(l-r)
-    // y, l and r are vectors of length N
-    // alpha is a scalar
-    void hpc_scaled_diff(Field& y, const double alpha,
-        Field const& l, Field const& r, const int N)
-    {
-        
-        for(int i = 0; i < N; i++)
-            y[i] = alpha * (l[i]-r[i]);
-    }
+ // copy one vector into another y := x
+ // x and y are vectors of length N
+ void hpc_copy(Field& y, Field const& x, const int N)
+ {
+     //TODO
+     for(int i = 0; i < N; i++)
+         y[i] = x[i];
+ }
 
-    // computes y := alpha*x
-    // alpha is scalar
-    // y and x are vectors on length n
-    void hpc_scale(Field& y, const double alpha, Field& x, const int N)
-    {
-        
-        for(int i = 0; i < N; i++)
-            y[i] = alpha * x[i];
-    }
+ // conjugate gradient solver
+ // solve the linear system A*x = b for x
+ // the matrix A is implicit in the objective function for the diffusion equation
+ // the value in x constitute the "first guess" at the solution
+ // x(N)
+ // ON ENTRY contains the initial guess for the solution
+ // ON EXIT  contains the solution
+ void hpc_cg(Field& x, Field const& b, const int maxiters, const double tol, bool& success)
+ {
+     // this is the dimension of the linear system that we are to solve
+     using data::options;
+     int N = options.N;
+     int nx = options.nx;
 
-    // computes linear combination of two vectors y := alpha*x + beta*z
-    // alpha and beta are scalar
-    // y, x and z are vectors on length n
-    void hpc_lcomb(Field& y, const double alpha, Field& x, const double beta,
-        Field const& z, const int N)
-    {
-        
-        for(int i = 0; i < N; i++)
-            y[i] = alpha * x[i] + beta * z[i];
-    }
+     if (!cg_initialized)
+         cg_init(nx);
 
-    // copy one vector into another y := x
-    // x and y are vectors of length N
-    void hpc_copy(Field& y, Field const& x, const int N)
-    {
-        
-        for(int i = 0; i < N; i++)
-            y[i] = x[i];
-    }
+     // epslion value use for matrix-vector approximation
+     double eps     = 1.e-8;
+     double eps_inv = 1. / eps;
 
-// conjugate gradient solver
-// solve the linear system A*x = b for x
-// the matrix A is implicit in the objective function for the diffusion equation
-// the value in x constitute the "first guess" at the solution
-// x(N)
-// ON ENTRY contains the initial guess for the solution
-// ON EXIT  contains the solution
-void hpc_cg(Field& x, Field const& b, const int maxiters, const double tol, bool& success)
-{
-    // this is the dimension of the linear system that we are to solve
-    using data::options;
-    int N = options.N;
-    int nx = options.nx;
+     // allocate memory for temporary storage
+     hpc_fill(Fx,    0.0, N);
+     hpc_fill(Fxold, 0.0, N);
+     hpc_copy(xold, x, N);
 
-    if (!cg_initialized)
-        cg_init(nx);
+     // matrix vector multiplication is approximated with
+     // A*v = 1/epsilon * ( F(x+epsilon*v) - F(x) )
+     //     = 1/epsilon * ( F(x+epsilon*v) - Fxold )
+     // we compute Fxold at startup
+     // we have to keep x so that we can compute the F(x+exps*v)
+     diffusion(x, Fxold);
 
-    // epslion value use for matrix-vector approximation
-    double eps     = 1.e-8;
-    double eps_inv = 1. / eps;
+     // v = x + epsilon*x
+     hpc_scale(v, 1.0 + eps, x, N);
 
-    // allocate memory for temporary storage
-    hpc_fill(Fx,    0.0, N);
-    hpc_fill(Fxold, 0.0, N);
-    hpc_copy(xold, x, N);
+     // Fx = F(v)
+     diffusion(v, Fx);
 
-    // matrix vector multiplication is approximated with
-    // A*v = 1/epsilon * ( F(x+epsilon*v) - F(x) )
-    //     = 1/epsilon * ( F(x+epsilon*v) - Fxold )
-    // we compute Fxold at startup
-    // we have to keep x so that we can compute the F(x+exps*v)
-    diffusion(x, Fxold);
+     // r = b - A*x
+     // where A*x = (Fx-Fxold)/eps
+     hpc_add_scaled_diff(r, b, -eps_inv, Fx, Fxold, N);
 
-    // v = x + epsilon*x
-    hpc_scale(v, 1.0 + eps, x, N);
+     // p = r
+     hpc_copy(p, r, N);
 
-    // Fx = F(v)
-    diffusion(v, Fx);
+     // r_old_inner = <r,r>
+     double r_old_inner = hpc_dot(r, r, N), r_new_inner = r_old_inner;
 
-    // r = b - A*x
-    // where A*x = (Fx-Fxold)/eps
-    hpc_add_scaled_diff(r, b, -eps_inv, Fx, Fxold, N);
+     // check for convergence
+     success = false;
+     if (sqrt(r_old_inner) < tol)
+     {
+         success = true;
+         return;
+     }
 
-    // p = r
-    hpc_copy(p, r, N);
+     int iter;
+     for(iter=0; iter<maxiters; iter++) {
+         // Ap = A*p
+         hpc_lcomb(v, 1.0, xold, eps, p, N);
+         diffusion(v, Fx);
+         hpc_scaled_diff(Ap, eps_inv, Fx, Fxold, N);
 
-    // r_old_inner = <r,r>
-    double r_old_inner = hpc_dot(r, r, N), r_new_inner = r_old_inner;
+         // alpha = r_old_inner / p'*Ap
+         double alpha = r_old_inner / hpc_dot(p, Ap, N);
 
-    // check for convergence
-    success = false;
-    if (sqrt(r_old_inner) < tol)
-    {
-        success = true;
-        return;
-    }
+         // x += alpha*p
+         hpc_axpy(x, alpha, p, N);
 
-    int iter;
-    for(iter=0; iter<maxiters; iter++) {
-        // Ap = A*p
-        hpc_lcomb(v, 1.0, xold, eps, p, N);
-        diffusion(v, Fx);
-        hpc_scaled_diff(Ap, eps_inv, Fx, Fxold, N);
+         // r -= alpha*Ap
+         hpc_axpy(r, -alpha, Ap, N);
 
-        // alpha = r_old_inner / p'*Ap
-        double alpha = r_old_inner / hpc_dot(p, Ap, N);
+         // find new norm
+         r_new_inner = hpc_dot(r, r, N);
 
-        // x += alpha*p
-        hpc_axpy(x, alpha, p, N);
+         // test for convergence
+         if (sqrt(r_new_inner) < tol) {
+             success = true;
+             break;
+         }
 
-        // r -= alpha*Ap
-        hpc_axpy(r, -alpha, Ap, N);
+         // p = r + r_new_inner.r_old_inner * p
+         hpc_lcomb(p, 1.0, r, r_new_inner / r_old_inner, p, N);
 
-        // find new norm
-        r_new_inner = hpc_dot(r, r, N);
+         r_old_inner = r_new_inner;
+     }
+     stats::iters_cg += iter + 1;
 
-        // test for convergence
-        if (sqrt(r_new_inner) < tol) {
-            success = true;
-            break;
-        }
+     if (!success)
+         std::cerr << "ERROR: CG failed to converge" << std::endl;
+ }
 
-        // p = r + r_new_inner.r_old_inner * p
-        hpc_lcomb(p, 1.0, r, r_new_inner / r_old_inner, p, N);
-
-        r_old_inner = r_new_inner;
-    }
-    stats::iters_cg += iter + 1;
-
-    if (!success)
-        std::cerr << "ERROR: CG failed to converge" << std::endl;
-}
-
-} // namespace linalg
+ } // namespace linalg
